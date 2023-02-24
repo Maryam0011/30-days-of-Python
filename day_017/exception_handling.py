7e0549bdf08529bc7d377e88a81d71de3d15058c
Maryam Sabo Abubakar <maryamsabubakar53@gmail.com>
2023-02-24 07:25:28 +0100

    Exception_handling

---
+++ day_017/exception_handling.py
@@ -0,0 +1,4 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries,es,ru = names
+print(f'The Nordic countries include: {nordic_countries} ')
+print(f'es: {es}, ru: {ru}')
